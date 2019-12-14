a830d505a2994ca4b6a74971e53efee5ddb5f73a
Chris Boyle <christopher.boyle.101@strath.ac.uk>
2019-12-14 22:56:24 +0000

    Semi-functional main()

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,37 @@
 #include <iostream>
 
+#include <string.h>
+
 #include "chain.hpp"
 
 
-int main(void)
+int main(int argc, const char** argv)
 {
-  BlockChain chain("path");
+  std::string path, store;
 
-  chain.store("hello_world");
-  chain.store("hello_world");
-  chain.store("hello_world");
-  chain.store("hello_world");
+  // TODO better error checking/input validation
+  for (int i = 1; i < argc; i++) {
 
+    if ((strcmp(argv[i], "-f") == 0) || (strcmp(argv[i], "--file") == 0))
+      path = argv[++i];
+    else if ((strcmp(argv[i], "-s") == 0) || (strcmp(argv[i], "--store") == 0))
+      store = argv[++i];
+    else
+      std::cerr 
+        << "unknown argument: \"" << argv[i] << "\"" << std::endl;
+  }
+
+  if (path.size() == 0) {
+    std::cerr << "missing argument: -f|--file -> the path to the blockchain file" << std::endl;
+    exit(1);
+  }
+
+  if (store.size() == 0) {
+    std::cerr << "missing argument: -s|--store -> the string blob to store in the chain" << std::endl;
+    exit(1);
+  }
+
+  BlockChain chain(path);
+  chain.store(store);
+  chain.write();
 }
